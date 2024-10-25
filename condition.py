e0e6c291666e6206130308e0c605e48ccc353394
Anugrah-Chandran <anugrahchandran2028@cs.sjcetpalai.ac.in>
2024-10-25 14:42:48 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,9 @@
+'''Author: Anugrah Chandran
+Date: 25-10-24
+Description: python program to find a number is greater or lesser than an another number'''
+number1=int(input("Enter your first number"))
+number2=int(input("Enter your second number"))
+if number1>number2:
+    print(number1," is greater than", number2)
+else :
+    print(number2, " is greater than", number1)
